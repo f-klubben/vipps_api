ac7bd4e865139c8ae466c5859a9e749ce95c8770
Kresten Laust <krestenlaust@gmail.com>
2025-04-18 18:16:54 +0200

    Move token loading to separate class

--- vipps_api.py
+++ vipps_api.py
@@ -27,6 +27,38 @@ class AccountingAPIKeys:
     client_secret: str
 
 
+class Utils(object):
+    logger = logging.getLogger(__name__)
+
+    tokens_file = (Path(__file__).parent / 'vipps-tokens.json').as_posix()
+    tokens_file_backup = (Path(__file__).parent / 'vipps-tokens.json.bak').as_posix()
+
+    @staticmethod
+    def load_accounting_keys_from_file(path: str) -> AccountingAPIKeys:
+        with open(path, 'r') as json_file:
+            raw_tokens = json.load(json_file)
+
+            if raw_tokens is None:
+                raise TokenFileException("Token file is None")
+
+            if 'client_id' not in raw_tokens:
+                raise TokenFileException("client_id missing")
+
+            if 'client_secret' not in raw_tokens:
+                raise TokenFileException("client_secret missing")
+
+            return AccountingAPIKeys(client_id=raw_tokens['client_id'], client_secret=raw_tokens['client_secret'])
+
+    @classmethod
+    def load_accounting_api_keys(cls) -> AccountingAPIKeys:
+        try:
+            return Utils.load_accounting_keys_from_file(cls.tokens_file)
+        except:
+            cls.logger.error("tokens file not correct. Reverting to backup")
+
+        return Utils.load_accounting_keys_from_file(cls.tokens_file_backup)
+
+
 @dataclass
 class ReportAPIAccessToken:
     """
@@ -42,9 +74,6 @@ class ReportAPI:
     api_endpoint = 'https://api.vipps.no'
     logger = logging.getLogger(__name__)
 
-    tokens_file = (Path(__file__).parent / 'vipps-tokens.json').as_posix()
-    tokens_file_backup = (Path(__file__).parent / 'vipps-tokens.json.bak').as_posix()
-
     tokens: AccountingAPIKeys
     session: ReportAPIAccessToken
     ledger_id: int | None
@@ -56,34 +85,6 @@ class ReportAPI:
 
     def load(self):
         self.session = self.__retrieve_access_token()
-
-    def __read_token_storage(self) -> AccountingAPIKeys:
-        """
-        Reads the token variable from disk
-        """
-        raw_tokens: Any
-        with open(self.tokens_file, 'r') as json_file:
-            raw_tokens = json.load(json_file)
-
-        if raw_tokens is None:
-            self.logger.error("read token from storage. 'tokens' is None. Reverting to backup tokens")
-
-            with open(self.tokens_file_backup, 'r') as json_file_backup:
-                raw_tokens = json.load(json_file_backup)
-
-        # Read tokens
-        if raw_tokens is None:
-            raise TokenFileException("Token file is None")
-
-        if 'client_id' not in raw_tokens:
-            self.logger.error("[__read_token_storage] 'client_id' is not defined in token file")
-            raise TokenFileException("client_id missing")
-
-        if 'client_secret' not in raw_tokens:
-            self.logger.error("[__read_token_storage] 'client_secret' is not defined in token file")
-            raise TokenFileException("client_secret missing")
-
-        return AccountingAPIKeys(client_id=raw_tokens['client_id'], client_secret=raw_tokens['client_secret'])
 
     def __retrieve_new_session(self) -> ReportAPIAccessToken:
         """
